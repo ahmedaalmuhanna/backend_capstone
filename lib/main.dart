44f44abc83ae6a536164c3f05324b0b11b01375b
Ahmed <ahmed.a.almuhanna@hotmail.com>
2022-08-25 08:44:10 +0300

    Adding Routing and Provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,31 @@
+import 'package:capstoe_frontend/pages/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-      body: Container(),
-    ));
+// Router
+    return MaterialApp.router(
+      routeInformationParser: _router.routeInformationParser,
+      routerDelegate: _router.routerDelegate,
+      routeInformationProvider: _router.routeInformationProvider,
+    );
   }
+
+  final _router = GoRouter(routes: [
+    GoRoute(
+        path: '/', builder: (context, state) => HomeScreen()), // home adress
+    // GoRoute(
+    //     path: '/signed_in',
+    //     builder: (context, state) =>
+    //         SignedIn(userName: state.extra as String)), // signed_in address
+    // the thirs address ..
+  ]);
 }
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class HomeScreen extends StatelessWidget {
+  HomeScreen({Key? key}) : super(key: key);
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Text("test"));
+  }
+}
